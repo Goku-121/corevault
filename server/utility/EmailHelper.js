dbef73e111d7d3fabb3eca54daea878fde809acc
Goku-121 <pranto1320@gmail.com>
2026-06-14 21:29:57 +0600

    fix: resend smtp

--- server/utility/EmailHelper.js
+++ server/utility/EmailHelper.js
@@ -2,17 +2,17 @@ const nodemailer = require('nodemailer');
 
 const EmailSend = async (EmailTo, EmailText, EmailSubject) => {
     let transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,       
-        secure: true,     
+        host: 'smtp.resend.com',
+        port: 465,
+        secure: true,
         auth: {
-            user: process.env.EMAIL_USER,
+            user: 'resend',
             pass: process.env.EMAIL_PASS
         }
     });
 
     let mailOptions = {
-        from: `CoreVault <${process.env.EMAIL_USER}>`,
+        from: 'CoreVault <onboarding@resend.dev>',
         to: EmailTo,
         subject: EmailSubject,
         text: EmailText
